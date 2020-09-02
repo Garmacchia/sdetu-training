dd3efc5923e997641d1338e9b798d1304f0d81f7
Garmacchia <70655458+Garmacchia@users.noreply.github.com>
2020-09-02 19:41:44 +0200

    Update HelloWorld.java
    adding code from GitHub

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
-		System.out.println("I´m adding code to my project");
+		System.out.println("IÂ´m adding code to my project");
+		System.out.println("Adding code from GitHub");
 	}
 }
